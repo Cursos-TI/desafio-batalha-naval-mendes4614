2e8ff28eeb4c004f3b662bc44c15a49eb21af3d6
mendes4614 <mendesfribeiro13@outlook.com>
2025-06-25 22:49:18 -0300

    mendes batalhaNaval.c
    criei a matriz

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
+#define linhas 5
+#define colunas 5
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+    int tabuleiro [linhas][colunas];
+     
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
